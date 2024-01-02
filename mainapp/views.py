7fbaa2f4283b4948c157eccf6bb55e28abf55250
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-01-02 23:43:19 +0200

    Imelement update & create & delete tags

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views import View
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
-from mainapp.models import Task
+from mainapp.models import Task, Tag
 
 
 class TodoListView(ListView):
@@ -22,3 +24,41 @@ class TaskUpdateView(UpdateView):
     model = Task
     fields = "__all__"
     success_url = reverse_lazy("home")
+
+
+class DeleteTaskView(DeleteView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("home")
+
+
+class TaskStatusChangeView(View):
+    success_url = reverse_lazy("home")
+
+    def get(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
+        task.is_done = not task.is_done
+        task.save()
+        return HttpResponseRedirect(self.success_url)
+
+
+class TagListView(ListView):
+    model = Tag
+
+
+class TagCrateView(CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("tag-list")
+
+
+class TagUpdateView(UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("tag-list")
+
+
+class TagDeleteView(DeleteView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("tag-list")
