4c33a54e9bc8fb12d68a04fb4047b57ee13216ec
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-01-02 23:42:56 +0200

    Add crispy forms

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
     "mainapp"
 ]
 
@@ -123,3 +124,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
