0d296acb6cc4290630f7fe99ae589ecba1939541
Ilya <38630970+IlyaGrynyshyn@users.noreply.github.com>
2024-01-03 18:13:17 +0200

    Delete mainapp directory

--- mainapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from mainapp.models import Task,Tag
-
-admin.site.register(Task),
-admin.site.register(Tag)
--- mainapp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from mainapp.views import TodoListView
-
-urlpatterns = [
-    path("", TodoListView.as_view(), name="home")
-]
